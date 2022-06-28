26b90a8ad797f4bfc1f99bf89fb45b98dd0bc89a
Matthew Bowley <m.bowley98@gmail.com>
2022-06-28 17:36:54 +0100

    bugfix

--- wrapper.c
+++ wrapper.c
@@ -1,8 +1,9 @@
 #include "wrapper.h"
 #include <aubio/aubio.h>
 
+// set all the values of fvec from an array of float32
 void fvec_set_buffer(fvec_t *fvec, smpl_t *buf) {
-    for (uint_t i = 0; i < sizeof(buf)/sizeof(smpl_t); i++) {
+    for (uint_t i = 0; i < fvec->length; i++) {
         fvec_set_sample(fvec, buf[i], i);
     }
 }
