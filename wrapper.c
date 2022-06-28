b989d2cf2c459903ea247f3c598a5cd76c03f174
Matthew Bowley <m.bowley98@gmail.com>
2022-06-28 17:48:45 +0100

    another bugfix

--- wrapper.c
+++ wrapper.c
@@ -4,7 +4,7 @@
 // set all the values of fvec from an array of float32
 void fvec_set_buffer(fvec_t *s, smpl_t *buf) {
   uint_t i;
-  for ( i = 0; i < s->length * sizeof(smpl_t); i++ )
+  for ( i = 0; i < s->length; i++ )
   {
     s->data[i] = buf[i];
   }
